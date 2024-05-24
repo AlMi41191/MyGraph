c288c4bd79d9793d71e3e3daaf85c90d48db84af
Alexander <134769734+AlMi41191@users.noreply.github.com>
2024-05-24 21:21:35 +0300

    Update Solution.java

--- src/main/java/graph/Solution.java
+++ src/main/java/graph/Solution.java
@@ -1,32 +1,38 @@
 package graph;
 
 public class Solution {
+    private static final int SIZE = 5;
+    
     public static void main(String[] args) {
         Vertex A = Vertex.createVertex('A', 0, 0);
-        Vertex B = Vertex.createVertex('B', 0, 4);
-        Vertex C = Vertex.createVertex('C', 4, 0);
-        Vertex D = Vertex.createVertex('D', 4, 4);
-        Vertex E = Vertex.createVertex('E', 2, 2);
-
-        Graph graph = new Graph(5);
-
+        Vertex B = Vertex.createVertex('B', 0, SIZE - 1);
+        Vertex C = Vertex.createVertex('C', SIZE - 1, 0);
+        Vertex D = Vertex.createVertex('D', SIZE - 1, SIZE - 1);
+        Vertex E = Vertex.createVertex('E', SIZE/2, SIZE/2);
+        
+        Graph graph = new Graph(SIZE);
+        
         graph.addVertex(A);
         graph.addVertex(B);
-        graph.addVertex(D);
-        graph.addVertex(C);
         graph.addVertex(E);
-
-        graph.addEdge(A, B);
-        graph.addEdge(B, D);
-        graph.addEdge(B, A);
-        graph.addEdge(D, C);
-        graph.addEdge(C, A);
-
-        System.out.println("Visits: ");
-
-        graph.dfs(A);
+        graph.addVertex(C);
+        graph.addVertex(D);
         
-        graph.printEdgeInt();
-        graph.printEdgeMap();
+        graph.addEdge(E, A);
+        graph.addEdge(E, B);
+        graph.addEdge(E, D);
+        graph.addEdge(E, C);
+        graph.addEdge(A, B);
+        graph.addEdge(A, C);
+        
+        // graph.dfsMap(A);
+        graph.dfsInt(A);
+        
+        graph.printAdjMap();
+        graph.printAdjMatrix();
+        
+        for (int i = 0; i < 6; i++) {
+            System.out.println(graph.getEdge(i));
+        }
     }
 }
